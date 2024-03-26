06dcef06bcd778c1d29ef810234a6d03e1966f5c
pawelce <szkolacecot@gmail.com>
2024-03-26 10:33:49 +0100

    siklssikla

---
+++ zad1/zad1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class Pojazdy {
+public:
+	string name_p;
+	void prezentacjaPojazdu() {
+		cout << "Nazwa pojazdu:" << this->name_p << endl;
+	}
+};
+class Samochody:public Pojazdy {
+public:
+	string name_s;
+	void prezentacjaSamochody() {
+		cout << "Nazwa samochodu:" << this->name_s << endl;
+	}
+					
+};
+class Rolnicze:public Samochody {
+public:
+	string name_r;
+	void prezentacjaRolniczych() {
+		cout << "Nazwa Pojazdu Rolniczego:" << this->name_r << endl;
+	}
+
+};
+
+
+int main()
+{
+	Rolnicze rol;
+	rol.name_p = "Bizon";
+	rol.name_s = "Szybki bizon";
+	rol.name_r = "Wiejski bizonek";
+	rol.prezentacjaPojazdu();
+	rol.prezentacjaSamochody();
+	rol.prezentacjaRolniczych();
+
+
+
+
+	return EXIT_SUCCESS;
+}
+
